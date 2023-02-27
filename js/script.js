7920142cc07da7aa27f67608d981d2cee7a13ec4
Anna Marczewska <aniqanet@gmail.com>
2023-02-27 14:17:44 +0100

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+console.log("Cześć!");
+
+let button1 = document.querySelector(".button1");
+let header__title = document.querySelector(".header__title");
+
+let sectionFour__button = document.querySelector(".sectionFour__button");
+let czwartaZwrotka = document.querySelector(".czwartaZwrotka");
+
+
+button1.addEventListener("click", () => {
+    header__title.remove();
+});
+
+sectionFour__button.addEventListener("click", () => {
+    czwartaZwrotka.innerText = "Wesołych Świąt HO HO HO";
+});
+
+let buttonBetweenSection = document.querySelector(".buttonBetweenSection");
+let body = document.querySelector(".body");
+let themeName = document.querySelector(".themeName");
+
+buttonBetweenSection.addEventListener("click", () => {
+    body.classList.toggle("dark");
+
+if (body.classList.contains("dark")) {
+    themeName.innerText = "Ciemny";
+  } else {
+    themeName.innerText = "Jasny";
+  }
+});
